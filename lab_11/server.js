87300d79c8a4f84d2d691ff00cecf37c8bab6af9
Chanuda2k2 <chanudathewmithuk@gmail.com>
2025-01-30 12:05:33 +0000

    update knock

--- lab_11/server.js
+++ lab_11/server.js
@@ -1,5 +1,8 @@
 var express = require('express');
+var knockknock = require('knock-knock-jokes');
 var app = express();
+
+
 app.get('/', function(req, res){
  res.send("Hello world! by express");
 });
